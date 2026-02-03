82665dc87115d379302fd7e791b7b4a75b4ec2fd
sarfaraz siddiqui <siddiqui.sarfaraz.developer@gmail.com>
2026-02-03 11:43:01 +0530

    my portfolio add map

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { FaDribbble, FaEnvelope, FaGithub, FaLinkedin, FaMapMarkerAlt, FaPhone, FaTwitter } from "react-icons/fa";
+import {
+  FaDribbble,
+  FaEnvelope,
+  FaGithub,
+  FaLinkedin,
+  FaMapMarkerAlt,
+  FaPhone,
+  FaTwitter,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function Contact() {
   return (
@@ -68,64 +77,92 @@ function Contact() {
           <div>
             {/* Contact Information */}
             <div className="flex py-9">
+              <div className="space-y-8 ">
+                <div className="flex items-start">
+                  <div className="text-purple text-2xl mr-4">
+                    <FaMapMarkerAlt />
+                  </div>
 
-            <div className="space-y-8 ">
-              <div className="flex items-start">
-                <div className="text-purple text-2xl mr-4">
-                  <FaMapMarkerAlt />
+                  <div>
+                    <h3 className="text-2xl font-semibold mb-2">Location</h3>
+                    <p className="text-gray-400">Navi Mumbai Taloja Phase 1</p>
+                  </div>
+                </div>
+                <div className="flex items-start">
+                  <div className="text-purple text-2xl mr-4">
+                    <FaEnvelope />
+                  </div>
+
+                  <div>
+                    <h3 className="text-2xl font-semibold mb-2">Email</h3>
+                    <p className="text-gray-400">
+                      Siddiquisarfaraz844@gmail.com
+                    </p>
+                  </div>
+                </div>
+                <div className="flex items-start">
+                  <div className="text-purple text-2xl mr-4">
+                    <FaPhone />
+                  </div>
+                  <div>
+                    <h3 className="text-2xl font-semibold mb-2">Phone</h3>
+                    <p className="text-gray-400">+91 9819650692</p>
+                  </div>
                 </div>
 
-                <div>
-                  <h3 className="text-2xl font-semibold mb-2">Location</h3>
-                  <p className="text-gray-400">Navi Mumbai Taloja Phase 1</p>
+                <div className="pt-4">
+                  <h3 className="text-lg font-semibold mb-4">Follow Me</h3>
+                  <div className="flex space-x-4">
+                    <a
+                      href="https://github.com/khankavi088-ai"
+                      className="w-12 h-12 rounded-full bg-dark-300 flex items-center justify-center
+                  text-purple hover:bg-purple hover:text-white transition duration-300"
+                    >
+                      <FaGithub />
+                    </a>
+
+                    <a
+                      href="https://www.linkedin.com/in/sarfaraz-siddiqui-2b36a1345/"
+                      className="w-12 h-12 rounded-full bg-dark-300 flex items-center justify-center
+                  text-blue hover:bg-blue hover:text-white transition duration-300"
+                    >
+                      <FaLinkedin />
+                    </a>
+
+                    {/* <a
+                      href="/"
+                      className="w-12 h-12 rounded-full bg-dark-300 flex items-center justify-center
+                  text-blue hover:bg-blue hover:text-white transition duration-300"
+                    >
+                      <FaTwitter />
+                    </a> */}
+
+                    {/* <a
+                      href="/"
+                      className="w-12 h-12 rounded-full bg-dark-300 flex items-center justify-center
+                  text-pink hover:bg-pink hover:text-white transition duration-300"
+                    >
+                      <FaDribbble />
+                    </a> */}
+                  </div>
                 </div>
-              </div>
-              <div className="flex items-start">
-                <div className="text-purple text-2xl mr-4">
-                  <FaEnvelope />
-                </div>
-
-                <div>
-                  <h3 className="text-2xl font-semibold mb-2">Email</h3>
-                  <p className="text-gray-400">Siddiquisarfaraz844@gmail.com</p>
-                </div>
-              </div>
-              <div className="flex items-start">
-                <div className="text-purple text-2xl mr-4">
-                  <FaPhone />
-                </div>
-                <div>
-                  <h3 className="text-2xl font-semibold mb-2">Phone</h3>
-                  <p className="text-gray-400">+91 9819650692</p>
-                </div>
-              </div>
-
-              <div className="pt-4">
-                <h3 className="text-lg font-semibold mb-4">Follow Me</h3>
-                <div className="flex space-x-4">
-                  <a
-                   href="#" className="w-12 h-12 rounded-full bg-dark-300 flex items-center justify-center
-                  text-purple hover:bg-purple hover:text-white transition duration-300"><FaGithub/></a>
-
-                   <a
-                    href="#" className="w-12 h-12 rounded-full bg-dark-300 flex items-center justify-center
-                  text-blue hover:bg-blue hover:text-white transition duration-300"><FaLinkedin/></a>
-
-                   <a
-                    href="/" className="w-12 h-12 rounded-full bg-dark-300 flex items-center justify-center
-                  text-blue hover:bg-blue hover:text-white transition duration-300"><FaTwitter/></a>
-
-                   <a
-                    href="/" className="w-12 h-12 rounded-full bg-dark-300 flex items-center justify-center
-                  text-pink hover:bg-pink hover:text-white transition duration-300"><FaDribbble/></a>
-
-                </div>
-
               </div>
             </div>
           </div>
         </div>
-            </div>
+      </div>
+      <div className="container mx-auto px-6 mt-20 max-w-5xl">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1885.342192812108!2d73.09016043829064!3d19.077609380427845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7ea0bc5ffabd9%3A0x8c640b190c476fbf!2sTaloja%20Phase%201%2C%20Taloja%20Panchanand%2C%20Taloja%2C%20Navi%20Mumbai%2C%20Maharashtra%20410208!5e0!3m2!1sen!2sin!4v1770095951041!5m2!1sen!2sin"
+          width="100%"
+          height="600"
+          style={{ border: 0 }}
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          title="Company Location Map"
+          className="rounded-lg shadow-md"
+        ></iframe>
       </div>
     </motion.div>
   );
